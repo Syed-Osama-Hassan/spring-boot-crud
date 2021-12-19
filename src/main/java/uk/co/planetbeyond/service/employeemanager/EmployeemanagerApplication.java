bfb3f1838c23c2ad079ee92722009dc279409b86
syed-osama-hassan <syedosamahassan16@gmail.com>
2021-12-19 23:52:49 +0500

    added CORS configuration

--- src/main/java/uk/co/planetbeyond/service/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/uk/co/planetbeyond/service/employeemanager/EmployeemanagerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeemanagerApplication {
+public class EmployeemanagerApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(EmployeemanagerApplication.class, args);
 	}
 
